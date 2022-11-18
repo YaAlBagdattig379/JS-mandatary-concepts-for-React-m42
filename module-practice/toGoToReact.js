09e164b5a2c8ba8f16a9d2a8aa975e092198bb55
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-11-18 18:21:03 +0530

    added

---
+++ module-practice/toGoToReact.js
@@ -0,0 +1,33 @@
+/**
+    ********************MUST HAVE TO PRACTICE FOR REACT**************************
+ * 
+ * 
+.1..find the documentaiton of console tab utilities API and then read it spend    time 30-40 minutes
+.2..easiest way to find out the event listener attached to a DOM element 
+.3..how can you break the first line of function  without actually adding a    breakpoint ? rather you will use a console api utilities.
+.4..go to source tab and add condition breakpoint . 
+
+--------------------------------___  \    /____ -------------------------------- 
+--------------------------------______\  /______-------------------------------- 
+--------------------------------_______\/_______-------------------------------- 
+
+.4..what is accessibility? 
+.5..what is call stack? 
+.6..what is the purposes of regular expression? 
+.7..what is the purpose of regular expression? 
+.8..what is the purpose of the lighthouse tab of chrome dev ? 
+.9..what is the purpose of the lighthouse tab of chrome dev ? 
+ * -----------------------------------------------------------------------------
+ * -----------------------------------------------------------------------------
+ * -----------------------------------------------------------------------------
+  ``````````````````````````tasks before go to REACT```````````````````````````` 
+  
+  A)
+    1...simple website. you hav a input and a button. input wil take an email.  use regular expression to valid the inserting an email.
+                         (find the regular expression by searching online )
+    2...if email is valid display some green color text and if not display ,
+                    show  some  error red color text. 
+  B) then go to p-hero github and download "issues tracker" to solve its problem
+      i) close button click korlei upoer data gulo cross hoa jabe
+      ii) delete button click korlei upoer data gulo clear hoa jabe
+ * */
